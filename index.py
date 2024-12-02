ecbbfa93afa001fc4630a83155a56a1e88119d19
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-12-02 21:37:24 +0000

    feat(learning): Variables and data types

--- index.py
+++ index.py
@@ -1,1 +1,18 @@
-#!/usr/bin/env python3.12
+# Variables
+nom = "Dieng" # string
+c = 'D' # string
+age = 25   # number
+taille = 1.99   # float
+complexe = 2 + 3j # complex number
+is_male = True  # boolean
+r = range(5)
+
+# Type de données
+print(type(nom)) # Output: <class'str'>
+print(type(c)) # Output: <class'str'>
+print(type(age)) # Output: <class 'int'>
+print(type(taille)) # Output: <class 'float'>
+print(type(complexe)) # Output: <class 'complex'>
+print(type(is_male)) # Output: <class 'bool'>
+
+print(r)
