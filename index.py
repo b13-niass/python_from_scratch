77dfe5e22d61fd25c9b1ec9772555bb61dcf87cc
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-12-03 00:58:11 +0000

    feat(learning):  Conditional statements (if, elif, else)

--- index.py
+++ index.py
@@ -1,10 +1,8 @@
-# Opérateurs logiques
-vrai = True
-faux = False
+age = 20
 
-et_logique = vrai & faux  # (and ou &) ET logique : False (les deux doivent être True)
-ou_logique = vrai | faux   # (or ou |) OU logique : True (au moins un doit être True)
-non_logique =  not vrai      # NON logique : False (inverse la valeur)
-
-print("\nOpérateurs logiques :")
-print(f"ET logique : {et_logique}, OU logique : {ou_logique}, NON logique : {non_logique}")
+if age < 18:
+    print("Vous êtes mineur.")
+elif age == 18:
+    print("Vous venez d'avoir 18 ans, félicitations !")
+else:
+    print("Vous êtes majeur.")
