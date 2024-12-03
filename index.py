b8981b5df4abf30225319d8eaa8866d28799a8f6
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-12-03 00:36:57 +0000

    feat(learning):  Opérateur Arithmetic

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
-# Ceci est un commentaire
-print("Bonjour, le monde !")  # Affiche un message
+# Opérateurs arithmétiques
+a = 10
+b = 3
 
-"""
-Ce programme affiche un message
-de bienvenue à l'utilisateur.
-"""
-print("Bienvenue !")
+addition = a + b        # Addition : 10 + 3 = 13
+soustraction = a - b    # Soustraction : 10 - 3 = 7
+multiplication = a * b  # Multiplication : 10 * 3 = 30
+division = a / b        # Division réelle : 10 / 3 = 3.333...
+modulo = a % b          # Modulo (reste) : 10 % 3 = 1
+puissance = a ** b      # Puissance : 10^3 = 1000
+division_entiere = a // b  # Division entière : 10 // 3 = 3
 
-# l'indentation
-if True:
-    print("Ligne avec espaces")
-    print("Ligne avec tabulations")
+print("Opérateurs arithmétiques :")
+print(f"Addition : {addition}, Soustraction : {soustraction}")
+print(f"Multiplication : {multiplication}, Division : {division}")
+print(f"Modulo : {modulo}, Puissance : {puissance}, Division entière : {division_entiere}")
