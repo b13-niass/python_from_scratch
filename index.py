8bb22b51c8a1368088605b7397b446bb6d0c2375
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-12-03 00:24:01 +0000

    feat(learning):  Input two values in the same line

--- index.py
+++ index.py
@@ -1,11 +1,6 @@
-try:
- name = input("Enter your name: ")
- age = float(input("Enter your age: "))
- if age.is_integer():
-    print("vrai")
- else:
-    print("faux")
-except ValueError:
-    print("Invalid input. Please enter a valid age.")
 
-print(f"Hello, {name}! You are {age} years old.")
+try:
+    a, b = input("Enter two values separated by a space: ").split(sep=",")
+    print(f"the sum of {a} and {b} is: {int(a) + int(b)}")
+except ValueError:
+    print("Invalid input. Please enter two integers separated by a space.")
