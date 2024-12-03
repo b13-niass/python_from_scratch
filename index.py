bd50d81ceb65a6ae8aa5dd58a5e508267efed2d3
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-12-03 01:13:21 +0000

    feat(learning):  'For' loop

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
-age = 20
 
-if age < 18:
-    print("Vous êtes mineur.")
-elif age == 18:
-    print("Vous venez d'avoir 18 ans, félicitations !")
-else:
-    print("Vous êtes majeur.")
+fruits = ["apple", "banana", 10, "date", "elderberry"]
+
+for fruit in fruits:
+    print(fruit)
+
+try:
+    for i in range(0, fruits.__len__(), 2):
+        print(fruits[i])
+except IndexError:
+    print("Error: Index out of range")
