a7e1d9133fc769c4719a3e2ce8cb268b0d3240b9
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-12-04 09:12:32 +0000

    feat(learning):  Classes et objets

--- index.py
+++ index.py
@@ -1,28 +1,5 @@
-# # --- Gestion des exceptions de base ---
-# try:
-#     numerateur = int(input("Entrez un numérateur : "))
-#     denominateur = int(input("Entrez un dénominateur : "))
-#     resultat = numerateur / denominateur
-#     print(f"Résultat : {resultat}")
-# except ZeroDivisionError:
-#     print("Erreur : Division par zéro impossible.")
-# except ValueError:
-#     print("Erreur : Vous devez entrer un nombre entier.")
-# except Exception as e:
-#     print(f"Erreur inattendue : {e}")
-# else:
-#     print("La division a été effectuée avec succès.")
-# finally:
-#     print("Fin du programme.")
-#
+from classes.Voiture import Voiture
 
-# --- Lever une exception manuellement ---
-def verifier_age(age):
-    if age < 0:
-        raise ValueError("L'âge ne peut pas être négatif.")
-    print(f"L'âge est : {age}")
-
-try:
-    verifier_age(-5)
-except ValueError as e:
-    print(f"Erreur : {e}")
+voiture1 = Voiture("Toyota", "Corolla", 2020)
+voiture1.demarrer()
+voiture1.demarrer()
